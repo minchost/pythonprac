10dffeed09bf7d478f62cc3563d30264afe58a1c
hyuna <pha2323@naver.com>
2022-10-26 00:23:57 +0900

    Initial commit

---
+++ dbmovie.py
@@ -0,0 +1,8 @@
+from pymongo import MongoClient
+import certifi
+ca = certifi.where()
+client = MongoClient('mongodb+srv://test:sparta@cluster0.vgprdjr.mongodb.net/Cluster0?retryWrites=true&w=majority', tlsCAFile=ca)
+db = client.dbsparta
+
+
+db.movies.update_one({'title':'가버나움'},{'$set':{'star':"0"}})
